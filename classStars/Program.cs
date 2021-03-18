c3f3f5fd16566ee3d37c5e6e096c469511ae531e
calugareanu.gloria@gmail.com <calugareanu.gloria@gmail.com>
2021-03-18 12:09:13 -0700

    classStars

---
+++ classStars/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classStars
+{
+    class Stars
+    {
+        int nrRows;
+        public Stars(int nrRows)
+        {
+            this.nrRows = nrRows;
+
+        }
+        public void drawTheStars()
+        {
+            for(int i=1;i <=nrRows;i++)
+            {
+                for (int j=1; j<=i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        ~Stars()
+        {
+            Console.WriteLine("Clasa distrusa");  //destructor
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Stars s = new Stars(10);  //numarul de coloane
+                s.drawTheStars();
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
